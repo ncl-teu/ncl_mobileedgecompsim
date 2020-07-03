8490c4850dba17b1869449475a62406a85befb33
kanemih <kanemitsuh@stf.teu.ac.jp>
2020-07-03 14:21:34 +0900

    Add files via upload

---
+++ mec.bat
@@ -0,0 +1,3 @@
+java -cp ./classes;lib/commons-math-2.0.jar;lib/ncl-sfc.jar;lib/ncl-taskschedsim.jar net.gripps.cloud.offload.main.Offload_RandomDAG_SingleUser cloud.properties
+
+pause;
--- mec.sh
+++ mec.sh
@@ -1,1 +1,1 @@
-java -cp ./classes:lib/commons-math-2.0.jar:lib/ncl-sfc.jar:lib/ncl-taskschedsim.jar net.gripps.cloud.offload.main.Offload_RandomDAG_SingleUser
+java -cp ./classes:lib/commons-math-2.0.jar:lib/ncl-sfc.jar:lib/ncl-taskschedsim.jar net.gripps.cloud.offload.main.Offload_RandomDAG_SingleUser cloud.properties
